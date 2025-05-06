9a596aede295417370658ed6f85db1a35087c2c0
zeroad00 <yryun00@gmail.com>
2025-05-06 17:30:51 +0900

    Time: 5 ms (72.94%), Space: 43.1 MB (93.89%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
@@ -19,3 +19,21 @@ class Solution {
         return equalMax;
     }
 }
+
+// 1ms 걸리는 코드
+// class Solution {
+//     public int findLucky(int[] arr) {
+//         int [] freq = new int[501];
+//         for(int num : arr){
+//             freq[num]++;
+//         }
+
+//         int max = -1;
+//         for(int i=1;i<freq.length;i++){
+//             if(freq[i]  == i){
+//                     max = Math.max(max,freq[i]); 
+//             }
+//         }
+//             return max;
+//     }
+// }
